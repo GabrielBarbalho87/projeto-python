56d3f3763b5a3af13019923046bbedfb0cc18742
GabrielBarbalho87 <barbalhogabriel06@gmail.com>
2024-09-13 13:49:36 -0300

    Abrir pasta sozinho

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-print('olá estou no meu primeiro projeto')
+#como usar a função click
+import pyautogui
+# Abrir explorador de arquivos
+pyautogui.click(x=1016,y=1049, clicks=1,
+interval=0.0, button='left', duration=2)
+# abrir a aba Area de trabalho
+pyautogui.click(x=122,y=349, clicks=1,
+interval=0.0, button='left', duration=2)
+# Apertar botão direito
+pyautogui.click(x=1649,y=379, clicks=1,
+interval=0.0, button='right', duration=2)
+# apertar botão Novo
+pyautogui.click(x=1478,y=561, clicks=1,
+interval=0.0, button='left', duration=2)
+# Abrir nova pasta 
+pyautogui.click(x=1086,y=90, clicks=1,
+interval=0.0, button='left', duration=2)
